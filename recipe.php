e3a85433773786e517e669707578dcaf86e5b7be
Markus2332 <azim2332@gmail.com>
2024-06-30 23:54:15 +0300

    Update recipe.php

--- recipe.php
+++ recipe.php
@@ -18,8 +18,39 @@ if (isset($_GET['id'])) {
     <p><strong>Cooking Time:</strong> <?php echo htmlspecialchars($recipe['cooking_time']); ?> minutes</p>
     <p><strong>Ingredients:</strong><br><?php echo nl2br(htmlspecialchars($recipe['ingredients'])); ?></p>
     <p><strong>Instructions:</strong><br><?php echo nl2br(htmlspecialchars($recipe['instructions'])); ?></p>
+    <?php if (is_logged_in()): ?>
+        <button class="btn btn-outline-primary like-button" data-recipe-id="<?php echo $recipe['id']; ?>">Like</button>
+    <?php endif; ?>
     <a href="index.php" class="btn btn-secondary">Back to Recipes</a>
 </div>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.like-button').addEventListener('click', function() {
+        var recipeId = this.getAttribute('data-recipe-id');
+        fetch('like_recipe.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'recipe_id=' + recipeId
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'liked') {
+                this.classList.remove('btn-outline-primary');
+                this.classList.add('btn-primary');
+                this.innerText = 'Liked';
+            } else if (data.status === 'unliked') {
+                this.classList.remove('btn-primary');
+                this.classList.add('btn-outline-primary');
+                this.innerText = 'Like';
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    });
+});
+</script>
 
 <?php
     else:
