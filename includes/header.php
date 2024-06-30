f175d61b9ab70e1bfcb87fda48d06a3647a031a1
Markus2332 <azim2332@gmail.com>
2024-06-30 23:52:44 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -38,16 +38,16 @@ require_once __DIR__ . '/functions.php';
     </nav>
 </header>
 <style>
-    body{
+
+ body{
         margin-top: 9rem !important;
     }
+        #mainNav a{
+                   color: #fff !important;
+
+        }
     #mainNav{
         background-color: #375755 !important;
-        color: #7fd4c0 !important;
-        font-family: "SB Heading", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, 
-            "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
-            "Segoe UI Symbol", "Noto Color Emoji" !important;
-        font-weight: 700 !important;
     }
 }
 </style>
